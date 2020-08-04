472b5276c2a835b0e1eb063d1d50dc76878823df
Evan099 <891837367@qq.com>
2020-08-04 21:32:21 +0800

    2020-8-4 at home：get请求改为后面带参数名  ?k=v

--- src/main/java/com/southwind/phone_store/Controller/PhoneHanler.java
+++ src/main/java/com/southwind/phone_store/Controller/PhoneHanler.java
@@ -21,14 +21,14 @@ public class PhoneHanler {
     }
 
     //根据类型查询所有手机
-    @GetMapping("/findByCategoryType/{categoryType}")
-    public ResultVO findByCategoryType(@PathVariable("categoryType") Integer categoryType){
+    @GetMapping("/findByCategoryType")
+    public ResultVO findByCategoryType(Integer categoryType){
         return ResultVOUtil.success(phoneService.findPhoneInfoVOByCategoryType(categoryType));
     }
 
     //根据phoneId查手机规格
-    @GetMapping("/findSpecsByPhoneId/{phoneId}")
-    public ResultVO findSpecsByPhoneId(@PathVariable("phoneId") Integer phoneId){
+    @GetMapping("/findSpecsByPhoneId")
+    public ResultVO findSpecsByPhoneId(Integer phoneId){
         return ResultVOUtil.success(phoneService.findSpecsByPhoneId(phoneId));
     }
 
